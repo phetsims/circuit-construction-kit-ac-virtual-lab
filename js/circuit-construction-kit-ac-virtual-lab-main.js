a1c76aacf98d359e8a5ca82213a49d24f90dffdc
samreid <reids@colorado.edu>
2021-01-11 21:01:53 -0700

    Use LabScreenView but without noncontact ammeters, see https://github.com/phetsims/circuit-construction-kit-common/issues/647

--- js/circuit-construction-kit-ac-virtual-lab-main.js
+++ js/circuit-construction-kit-ac-virtual-lab-main.js
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2019-2020, University of Colorado Boulder
 
 /**
  * Main entry point for the sim.
@@ -6,33 +6,44 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
+// Image is required for making toDataURLNodeSynchronous work in the built version
+import CCKCOptionsDialogContent from '../../circuit-construction-kit-common/js/view/CCKCOptionsDialogContent.js';
 import Sim from '../../joist/js/Sim.js';
 import simLauncher from '../../joist/js/simLauncher.js';
+import soundManager from '../../tambo/js/soundManager.js';
 import Tandem from '../../tandem/js/Tandem.js';
-import CircuitConstructionKitAcVirtualLabScreen from './circuit-construction-kit-ac-virtual-lab/CircuitConstructionKitAcVirtualLabScreen.js';
 import circuitConstructionKitAcVirtualLabStrings from './circuitConstructionKitAcVirtualLabStrings.js';
+import LabScreen from '../../circuit-construction-kit-ac/js/lab/LabScreen.js';
+import '../../scenery/js/nodes/Image.js';
 
-const circuitConstructionKitAcVirtualLabTitleString = circuitConstructionKitAcVirtualLabStrings[ 'circuit-construction-kit-ac-virtual-lab' ].title;
+const circuitConstructionKitAcTitleString = circuitConstructionKitAcVirtualLabStrings[ 'circuit-construction-kit-ac-virtual-lab' ].title;
+
+// constants
+const tandem = Tandem.ROOT;
 
 const simOptions = {
-
-  //TODO fill in credits, all of these fields are optional, see joist.CreditsNode
+  createOptionsDialogContent: tandem => new CCKCOptionsDialogContent( tandem ),
   credits: {
-    leadDesign: '',
-    softwareDevelopment: '',
-    team: '',
-    qualityAssurance: '',
-    graphicArts: '',
-    soundDesign: '',
-    thanks: ''
+    leadDesign: 'Amy Rouinfar',
+    softwareDevelopment: 'Sam Reid, Denzell Barnett',
+    team: 'Michael Dubson, Ariel Paul, Kathy Perkins, Wendy Adams, Carl Wieman',
+    qualityAssurance: 'Jaspe Arias, Steele Dalton, Amanda Davis, Alex Dornan, Bryce Griebenow, Ethan Johnson, Liam Mulhall, Ben Roberts, Jacob Romero, Ethan Ward, Kathryn Woessner',
+    graphicArts: 'Bryce Gruneich, Mariah Hermsmeyer, Cheryl McCutchan'
   }
 };
 
 // launch the sim - beware that scenery Image nodes created outside of simLauncher.launch() will have zero bounds
 // until the images are fully loaded, see https://github.com/phetsims/coulombs-law/issues/70
 simLauncher.launch( () => {
-  const sim = new Sim( circuitConstructionKitAcVirtualLabTitleString, [
-    new CircuitConstructionKitAcVirtualLabScreen( Tandem.ROOT.createTandem( 'circuitConstructionKitAcVirtualLabScreen' ) )
+  const sim = new Sim( circuitConstructionKitAcTitleString, [
+    new LabScreen( tandem.createTandem( 'labScreen' ), {
+      labScreenViewOptions: {
+        showNoncontactAmmeters: false
+      }
+    } )
   ], simOptions );
   sim.start();
+
+  // turn off common UI sounds until a sound design has been done for this sim
+  soundManager.setOutputLevelForCategory( 'user-interface', 0 );
 } );
